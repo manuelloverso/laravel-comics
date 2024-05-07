ac1401fd4e9677dbf13de8535ece1c7e6722bb1f
Manuel <manuelloverso60@gmail.com>
2024-05-07 17:18:11 +0200

    add jumbotron

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    <h1>This is the header</h1>
+    <nav>
+        <ul class="list-unstyled d-flex ">
+            <li>some link</li>
+            <li>some link</li>
+            <li>some link</li>
+            <li>some link</li>
+            <li>some link</li>
+        </ul>
+    </nav>
+    <div class="jumbotron">
+        <img class="w-100" src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+    </div>
 </header>
