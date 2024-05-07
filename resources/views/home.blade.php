2c89e776f2ba64eac42574c160f7f79203c7e37d
Manuel <manuelloverso60@gmail.com>
2024-05-07 16:05:39 +0200

    add comic books

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>this is the home page main</h1>
+<div class="container">
+    <div class="row">
+        @foreach($comics as $comic)
+        <div class="col-3 g-4">
+            <div class="card h-100">
+                <div class="card-img">
+                    <img class="w-100 thumb" src="{{$comic['thumb']}}" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="text-uppercase">{{$comic['series']}}</h5>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
